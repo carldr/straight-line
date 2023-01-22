18ad17d4d7be1d0fe92f1344e4f5affbfcdbbe99
Carl Drinkwater <carl@29degrees.co.uk>
2023-01-22 14:02:43 +0000

    It's working, and should be returning the line which stays closest to the straight line.  Next development is to prefer lines which minimise the area between the line and the route. Can we use the area of the polygon(s) formed by the straight line and the route for this??

--- run.py
+++ run.py
@@ -15,10 +15,15 @@ import straightline
 def do_node( graph, start_node, end_node, count = 10 ):
   this_end_node_straight_path = None
   this_end_node_variation = None
+  this_end_node_route_length = None
   
   paths = []
+  variations = []
+  lengths = []
 
   #short_path = ox.shortest_path( graph, start_node, end_node, weight = "length" )
+ 
+  #  TODO: Can this method return distances?
   for short_path in ox.k_shortest_paths( graph, start_node, end_node, count, weight = "length" ):
     #if short_path:
     straight_line = LineString( [ Point( graph.nodes[ start_node ][ 'x' ], graph.nodes[ start_node ][ 'y' ] ), Point( graph.nodes[ end_node ][ 'x' ], graph.nodes[ end_node ][ 'y' ] ) ] )
@@ -37,22 +42,57 @@ def do_node( graph, start_node, end_node, count = 10 ):
 
       if dist > absolute_max_dist:
         absolute_max_dist = dist
-        
-    print( "  Worst distance : {}".format( absolute_max_dist ) )
+
+    route_length = sum( ox.utils_graph.get_route_edge_attributes( graph, short_path, "length" ) )
+
+    print( "  Worst variation : {}, distance : {}m".format( absolute_max_dist, route_length ) )
 
     if this_end_node_straight_path == None or absolute_max_dist < this_end_node_variation:
       print( "BEST THIS NODE" )
       this_end_node_straight_path = short_path
       this_end_node_variation = absolute_max_dist
+      this_end_node_route_length = route_length
 
     paths.append( short_path )
+    lengths.append( route_length )
+    variations.append( absolute_max_dist )
 
-  return this_end_node_straight_path, this_end_node_variation, paths
+
+  #
+  #  Now, for all routes we've seen, if the ending variation is less than about 10cm metre (0.000001 longitude at equator) then
+  #  prefer distance  
+  #
+  additional_path = None
+
+  #
+  #  TODO: k_shortest_paths returns paths in distance order, so we can do this in the loop above
+  #
+
+  for i in range(len(paths)):
+    if this_end_node_variation - variations[i] > 0.000001:
+      continue
+
+    if lengths[ i ] < this_end_node_route_length:
+      print( "BEST THIS NODE ON LENGTH" )
+      print( "Old: variation = {}, length = {}".format( this_end_node_variation, this_end_node_route_length ) )
+      print( "New: variation = {}, length = {}".format( variations[ i ], lengths[ i ] ) )
+
+      additional_path = this_end_node_straight_path
+
+      # TODO: See how zip() works
+      this_end_node_straight_path = paths[ i ]
+      this_end_node_variation = variations[ i ]
+      this_end_node_route_length = lengths[ i ]
+
+  return this_end_node_straight_path, this_end_node_variation, this_end_node_route_length, paths, additional_path
+
+
 
 def do_start_node( deets ):
   graph, s_idx, start_node, minimum_distance, boundary_nodes = deets
 
   paths = []
+  straightest_path_route_length = None
   straightest_path_variation = None
   straightest_path = None
 
@@ -74,7 +114,7 @@ def do_start_node( deets ):
       print( "  Great circle distance below minimum" )
       continue
 
-    this_end_node_straight_path, this_end_node_variation, _ = do_node( graph, start_node, end_node )
+    this_end_node_straight_path, this_end_node_variation, this_end_node_route_length, _, _ = do_node( graph, start_node, end_node )
     if this_end_node_straight_path == None:
       continue
 
@@ -82,10 +122,16 @@ def do_start_node( deets ):
       print( "  BEST" )
       straightest_path = this_end_node_straight_path
       straightest_path_variation = this_end_node_variation
+      straightest_path_route_length = this_end_node_route_length
 
     paths.append( { "path" : this_end_node_straight_path, "width" : 0.3, "colour" : "red" } )
 
-  return { "paths" : paths, "straightest_path" : straightest_path, "straightest_path_variation" : straightest_path_variation }
+  return {
+    "paths" : paths,
+    "straightest_path" : straightest_path,
+    "straightest_path_route_length" : straightest_path_route_length,
+    "straightest_path_variation" : straightest_path_variation
+  }
 
 
 def main( argv ):
@@ -96,9 +142,9 @@ def main( argv ):
   filename = "whitchurch.png"
   activity = "bike"
 
-  #relation = "R4581086"
-  #filename = "shrewsbury.png"
-  #activity = "walk"
+  relation = "R4581086"
+  filename = "shrewsbury.png"
+  activity = "walk"
 
   #relation = "R146656"
   #filename = "manchester.png"
@@ -184,9 +230,6 @@ def main( argv ):
 
   ##############################################################
 
-  paths = []
-  straightest_path_variation = None
-  straightest_path = None
 
 
   pool = multiprocessing.Pool( processes = 16 )
@@ -195,13 +238,20 @@ def main( argv ):
     jobs.append( ( graph, s_idx, start_node, minimum_distance, boundary_nodes ) )
 
   return_values = pool.map( do_start_node, jobs )
+  pool.close()
+  pool.join()
 
 
+  paths = []
+  straightest_path_variation = None
+  straightest_path = None
+  straightest_path_length = None
 
   for node in return_values:
     s_paths = node[ "paths" ]
     s_straightest_path = node[ "straightest_path" ]
     s_path_variation = node[ "straightest_path_variation" ]
+    s_path_length = node[ "straightest_path_route_length" ]
 
     for s_path in s_paths:
       paths.append( s_path )
@@ -210,18 +260,29 @@ def main( argv ):
       continue
 
     if straightest_path_variation == None or s_path_variation < straightest_path_variation:
+      print( "RET BEST" )
       straightest_path = s_straightest_path
       straightest_path_variation = s_path_variation
+      straightest_path_length = s_path_length
 
-  sleep( 2 )
-
-  print( "Doing the best 100 routes for the best found so far" )
-  straightest_path, straightest_path_variation, these_paths = do_node( graph, straightest_path[0], straightest_path[-1], 100 )
+  print( "Doing the best 500 routes for the best found so far" )
+  straightest_path, straightest_path_variation, straight_path_route_length, these_paths, additional_path = do_node(
+    graph,
+    straightest_path[0],
+    straightest_path[-1],
+    500
+  )
 
   for path in these_paths:
     paths.append( { "path" : path, "width" : 0.2, "colour" : "orange" } )
 
   paths.append( { "path" : straightest_path, "colour" : "green", "width" : 1.0, "line" : True } )
+
+  #  This was the path which was beaten on distance, with the same variation
+  if additional_path:
+    print( "PURPLE" )
+    paths.append( { "path" : additional_path, "width" : 0.5, "colour" : "purple" } )
+
 
   straightline.draw_paths( graph, boundary, boundary_gdf, paths, activity + "-" + filename )
 
